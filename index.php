c9b9109315b99c40c199613e956e70142f8a6192
KARAJE.23343 <Karajedenis@gmail.com>
2026-05-05 11:24:41 +0200

    add viewport meta tag for improved responsive design

--- index.php
+++ index.php
@@ -173,6 +173,7 @@ if ($autenticato) {
 <html lang="it">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Karaje Gym</title>
     <link rel="stylesheet" href="assets/style.css">
     <link rel="icon" href="assets/icon.ico">
