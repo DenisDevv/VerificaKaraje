fc3f44ebfcbc31154d27f5fe02047ad13eb7eb0a
KARAJE.23343 <Karajedenis@gmail.com>
2026-05-05 10:47:28 +0200

    add link to external CSS stylesheet for improved styling

--- db.php
+++ db.php
@@ -226,6 +226,7 @@ if ($autenticato) {
 <head>
     <meta charset="UTF-8">
     <title>Karaje Gym</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 <?php if (!$autenticato): ?>
